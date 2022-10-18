064082f1884efb052861f6f27f7b94fb4479b018
aimme <muhammadhu.aiman@gmail.com>
2022-10-18 22:45:49 +0500

    authentication setup

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PhotoController;
-use App\Http\Controllers\ListingController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\LocationController;
-use App\Http\Controllers\CategoryProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,24 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/test', [HomeController::class, 'test'])->name('home.test');
-Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('d', [CategoryController::class, 'downloadCategoriesFromIbay'])->name('categories.downloadCategoriesFromIbay');
-Route::get('categories', [CategoryController::class, 'index'])->name('categories.index');
-
-// Route::group(['middleware' => ['auth'], function() {
-    Route::get('/categories/{slug}/products', [CategoryProductController::class, 'index'])->name('categories.products.index');
-    Route::get('/categories/for-select', [CategoryController::class, 'forSelect'])->name('categories.for-select');
-    Route::get('/categories/{id}/levels', [CategoryController::class, 'levels'])->name('categories.levels');
-    Route::get('parent-categories', [CategoryController::class, 'parents'])->name('categories.parents');
-    Route::get('categories/{id}/children', [CategoryController::class, 'children'])->name('categories.children');
-    Route::get('categories/{id}', [CategoryController::class, 'show'])->name('categories.show');
-    Route::post('photos', [PhotoController::class, 'store'])->name('photos.store');
-    Route::get('listings', [ListingController::class, 'index'])->name('listings.index');
-    Route::get('listings/create', [ListingController::class, 'create'])->name('listings.create');
-    Route::post('listings', [ListingController::class, 'store'])->name('listings.store');
-    Route::get('/locations/for-select', [LocationController::class, 'forSelect'])->name('locations.for-select');
-// });
+require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,20 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/js/**/*.{vue,js}',
     ],
 
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-                circular: ['Circular', ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio')
-    ],
+    plugins: [require('@tailwindcss/forms')],
 };
