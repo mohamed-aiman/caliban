db123addf95309758642c0f1a6bc1df77fb85d35
Mohamed Aiman <muhammadhu.aiman@gmail.com>
2022-10-31 16:18:06 +0500

    Create LocationService.js

---
+++ resources/js/services/LocationService.js
@@ -0,0 +1,17 @@
+import { BaseService } from '@/services/base.service'
+import { ErrorWrapper, ResponseWrapper } from '@/services/util'
+
+export class CategoryService extends BaseService {
+ 
+  static async forSelect (query) {
+    try {
+      const url = (query) ? `/locations/for-select?search=${query}` : `/locations/for-select`;
+      const response = await this.request({ auth: false }).get(url)
+      return new ResponseWrapper(response, response.data)
+    } catch (error) {
+      const message = error.response.data ? error.response.data.error : error.response.statusText
+      throw new ErrorWrapper(error, message)
+    }
+  }
+
+}
