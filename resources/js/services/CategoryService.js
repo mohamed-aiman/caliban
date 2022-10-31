fce2b2e3c05d5031048587beee5e98d1bc074e36
Mohamed Aiman <muhammadhu.aiman@gmail.com>
2022-10-31 16:14:55 +0500

    Create CategoryService.js

---
+++ resources/js/services/CategoryService.js
@@ -0,0 +1,47 @@
+import { BaseService } from '@/services/base.service'
+import { ErrorWrapper, ResponseWrapper } from '@/services/util'
+
+export class CategoryService extends BaseService {
+
+  static async parents (type) {
+    try {
+      const url = (type) ? `/api/parent-categories?type=${type}` : `/api/parent-categories`
+      const response = await this.request({ auth: false }).get(url)
+      return new ResponseWrapper(response, response.data)
+    } catch (error) {
+      const message = error.response.data ? error.response.data.error : error.response.statusText
+      throw new ErrorWrapper(error, message)
+    }
+  }
+
+  static async children (id) {
+    try {
+      const response = await this.request({ auth: false }).get(`/categories/${id}/children`)
+      return new ResponseWrapper(response, response.data)
+    } catch (error) {
+      const message = error.response.data ? error.response.data.error : error.response.statusText
+      throw new ErrorWrapper(error, message)
+    }
+  }
+  
+  static async levels (id) {
+    try {
+      const response = await this.request({ auth: false }).get(`/categories/${id}/levels`)
+      return new ResponseWrapper(response, response.data)
+    } catch (error) {
+      const message = error.response.data ? error.response.data.error : error.response.statusText
+      throw new ErrorWrapper(error, message)
+    }
+  }
+  
+  static async forSelect (query) {
+    try {
+      const response = await this.request({ auth: false }).get(`/categories/for-select?search=${query}`)
+      return new ResponseWrapper(response, response.data)
+    } catch (error) {
+      const message = error.response.data ? error.response.data.error : error.response.statusText
+      throw new ErrorWrapper(error, message)
+    }
+  }
+
+}
