ec22a23a1c8b09d196806075cc9f23d612672faf
Mohamed Aiman <muhammadhu.aiman@gmail.com>
2022-11-01 02:06:08 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,44 +20,45 @@ use App\Http\Controllers\CategoryProductController;
 |
 */
 
-
-// Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/api/search', [HomeController::class, 'search'])->name('search');
-// Route::get('/search', [HomeController::class, 'search'])->name('search');
+#DELETE THESE ROUTES LATER
 Route::get('/test', [HomeController::class, 'test'])->name('home.test');
-Route::get('/api/products/{slug}', [ProductController::class, 'show'])->name('products.show');
-// Route::get('/products/{slug}', [ProductController::class, 'show'])->name('products.show');
-Route::get('/api/products', [ProductController::class, 'index'])->name('products.index');
-// Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/api/parent-categories', [CategoryController::class, 'parents'])->name('categories.parents');
-// Route::get('parent-categories', [CategoryController::class, 'parents'])->name('categories.parents');
+Route::get('categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/{slug}/products', [CategoryProductController::class, 'index'])->name('categories.products.index');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('d', [CategoryController::class, 'downloadCategoriesFromIbay'])->name('categories.downloadCategoriesFromIbay');
-Route::get('categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::get('/categories/{slug}/products', [CategoryProductController::class, 'index'])->name('categories.products.index');
+//GUEST API ROUTES
+Route::group(['prefix'=>'api','as'=>'guest.'], function(){
+    Route::get('search', [HomeController::class, 'search'])->name('search');
+    Route::get('products/{slug}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('parent-categories', [CategoryController::class, 'parents'])->name('categories.parents');
+});
 
+//LOGGED IN USER API ROUTES
+Route::group(['middleware' => ['auth'], 'prefix'=>'api', 'as'=>'user.'], function(){
+    Route::get('categories/for-select', [CategoryController::class, 'forSelect'])->name('categories.for-select');
+    Route::get('categories/{id}/levels', [CategoryController::class, 'levels'])->name('categories.levels');
+    Route::get('categories/{id}/children', [CategoryController::class, 'children'])->name('categories.children');
+    Route::get('categories/{id}', [CategoryController::class, 'show'])->name('categories.show');
+    Route::post('photos', [PhotoController::class, 'store'])->name('photos.store');
+    Route::get('listings', [ListingController::class, 'index'])->name('listings.index');
+    Route::get('listings/{productSlug}/form-data', [ListingController::class, 'productFormData'])->name('listings.product-form-data');
+    Route::patch('listings/{productSlug}', [ListingController::class, 'update'])->name('listings.update');
+    Route::post('listings', [ListingController::class, 'store'])->name('listings.store');
+    Route::get('listings/{productSlug}', [ListingController::class, 'show'])->name('listings.show');
+    Route::get('locations/for-select', [LocationController::class, 'forSelect'])->name('locations.for-select');
+});
+
+//LOGGED IN USER SPA ROUTES
 Route::group(['middleware' => ['auth']], function() {
-    Route::get('/api/categories/for-select', [CategoryController::class, 'forSelect'])->name('categories.for-select');
-    Route::get('/api/categories/{id}/levels', [CategoryController::class, 'levels'])->name('categories.levels');
-    Route::get('/api/categories/{id}/children', [CategoryController::class, 'children'])->name('categories.children');
-    Route::get('/api/categories/{id}', [CategoryController::class, 'show'])->name('categories.show');
-    Route::post('/api/photos', [PhotoController::class, 'store'])->name('photos.store');
-    Route::get('/api/listings', [ListingController::class, 'index'])->name('listings.index');
-    // Route::get('listings/create', [ListingController::class, 'create'])->name('listings.create');
-    // Route::get('listings/{productSlug}/edit', [ListingController::class, 'edit'])->name('listings.edit');
-    Route::get('/api/listings/{productSlug}/form-data', [ListingController::class, 'productFormData'])->name('listings.product-form-data');
-    Route::patch('/api/listings/{productSlug}', [ListingController::class, 'update'])->name('listings.update');
-    Route::post('/api/listings', [ListingController::class, 'store'])->name('listings.store');
-    Route::get('/api/listings/{productSlug}', [ListingController::class, 'show'])->name('listings.show');
-    Route::get('/api/locations/for-select', [LocationController::class, 'forSelect'])->name('locations.for-select');
-    
     Route::view('/dashboard', 'spa-dashboard')->where('any', '.*');
     Route::view('/dashboard/{any}', 'spa-dashboard')->where('any', '.*');
 });
 
 require __DIR__.'/auth.php';
+
+//GUEST SPA ROUTES
 Route::view('/{any}', 'spa')->where('any', '.*');
