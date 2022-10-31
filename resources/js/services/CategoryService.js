b959268b0531c0c27a3f0ded5f0695c0c3ecc974
Mohamed Aiman <muhammadhu.aiman@gmail.com>
2022-10-31 16:28:30 +0500

    Update CategoryService.js

--- resources/js/services/CategoryService.js
+++ resources/js/services/CategoryService.js
@@ -3,6 +3,16 @@ import { ErrorWrapper, ResponseWrapper } from '@/services/util'
 
 export class CategoryService extends BaseService {
 
+  static async parents (id) {
+    try {
+      const response = await this.request({ auth: false }).get(`/categories/${id}`)
+      return new ResponseWrapper(response, response.data)
+    } catch (error) {
+      const message = error.response.data ? error.response.data.error : error.response.statusText
+      throw new ErrorWrapper(error, message)
+    }
+  }
+    
   static async parents (type) {
     try {
       const url = (type) ? `/api/parent-categories?type=${type}` : `/api/parent-categories`
