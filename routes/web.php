90dfed896e34d162c4ed8481ac3f1dce3fcef68b
Mohamed Aiman <mohamed.aiman@ncit.gov.mv>
2022-12-07 17:17:24 +0500

    s

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LikeController;
-use App\Http\Controllers\PhotoController;
-use App\Http\Controllers\ListingController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\LocationController;
-use App\Http\Controllers\CategoryProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,44 +25,18 @@ Route::get('/test', [HomeController::class, 'test'])->name('home.test');
 Route::get('/design', function() {
     return view('design');
 })->name('design');
-Route::get('categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::get('/categories/{slug}/products', [CategoryProductController::class, 'index'])->name('categories.products.index');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('redirect-to-sell', [ListingController::class, 'create'])->name('listings.create');
 //GUEST API ROUTES
 Route::group(['prefix'=>'api','as'=>'guest.'], function(){
-    Route::get('search', [ProductController::class, 'search'])->name('search');
-    Route::get('products/{slug}', [ProductController::class, 'show'])->name('products.show');
-    Route::get('products', [ProductController::class, 'index'])->name('products.index');
-    Route::get('parent-categories', [CategoryController::class, 'parents'])->name('categories.parents');
 
 });
 
 //LOGGED IN USER API ROUTES
 Route::group(['middleware' => ['auth'], 'prefix'=>'api', 'as'=>'user.'], function(){
-    Route::get('categories/for-select', [CategoryController::class, 'forSelect'])->name('categories.for-select');
-    Route::get('categories/{id}/levels', [CategoryController::class, 'levels'])->name('categories.levels');
-    Route::get('categories/{id}/children', [CategoryController::class, 'children'])->name('categories.children');
-    Route::get('categories/{id}', [CategoryController::class, 'show'])->name('categories.show');
-    Route::post('photos', [PhotoController::class, 'store'])->name('photos.store');
-    Route::get('listings', [ListingController::class, 'index'])->name('listings.index');
-    Route::get('listings/{productSlug}/form-data', [ListingController::class, 'productFormData'])->name('listings.product-form-data');
-    Route::patch('listings/is-active-toggle', [ListingController::class, 'isActiveToggle'])->name('listings.is-active-toggle');
-    Route::patch('listings/decrement-quantity', [ListingController::class, 'decrementQuantity'])->name('listings.decrement-quantity');
-    Route::patch('listings/increment-quantity', [ListingController::class, 'incrementQuantity'])->name('listings.increment-quantity');
-    Route::patch('listings/update-quantity', [ListingController::class, 'updateQuantity'])->name('listings.update-quantity');
-    Route::patch('listings/update-price', [ListingController::class, 'updatePrice'])->name('listings.update-price');
-    Route::patch('listings/{productSlug}', [ListingController::class, 'update'])->name('listings.update');
-    Route::post('listings', [ListingController::class, 'store'])->name('listings.store');
-    Route::get('listings/{productSlug}', [ListingController::class, 'show'])->name('listings.show');
-    Route::get('locations/for-select', [LocationController::class, 'forSelect'])->name('locations.for-select');
-    Route::get('watchlist', [LikeController::class, 'index'])->name('likes.index');
-    Route::post('likes/toggle', [LikeController::class, 'toggle'])->name('likes.toggle');
-
 });
 
 //LOGGED IN USER SPA ROUTES
